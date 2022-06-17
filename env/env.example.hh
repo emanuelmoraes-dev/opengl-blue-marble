d8f847fe4688bded859872c7feb38912400c758d
Emanuel Moraes de Almeida <emanuelmoraes297@gmail.com>
2022-06-16 22:52:07 -0300

    comment on BM_ENV_SHADERS_DIR

--- env/env.example.hh
+++ env/env.example.hh
@@ -1,6 +1,7 @@
 #ifndef _BLUE_MARBLE_ENV_HH_INCLUDED_
 #define _BLUE_MARBLE_ENV_HH_INCLUDED_
 
-#define BM_ENV_SHADERS_DIR "."
+// Folder path that has the shading source
+#define BM_ENV_SHADERS_DIR "shaders"
 
 #endif
