51ad3eb707e9d9434cdfde2b7dadc90c5bb7acff
Emanuel Moraes <emanuelmoraes297@gmail.com>
2023-01-01 19:01:42 -0300

    add specular light

--- shaders/triangule_frag.glsl
+++ shaders/triangule_frag.glsl
@@ -17,6 +17,13 @@ void main() {
     vec3 l = -normalize(LIGHT_DIRECTION);
     float lambertian = max(dot(n, l), 0.0);
 
+    vec3 v = vec3(0.0, 0.0, 1.0);
+    vec3 r = reflect(-l, n);
+    float alpha = 50.0; // ex: 2, 10, 50, 250, 1250
+    float specularIntensity = 0.60;
+    float specular = pow(max(dot(r, v), 0.0), alpha);
+    specular = max(specular, 0.0) * specularIntensity;
+
     vec3 texColor = texture(TEX_EARTH, uv).rgb;
-    outColor = vec4(texColor * LIGHT_INTENSITY * lambertian, 1.0f);
+    outColor = vec4(texColor * LIGHT_INTENSITY * lambertian + specular, 1.0f);
 }
